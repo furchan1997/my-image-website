7cee34154b2a5000a6f4d34e2e3edbd93efd5967
ariel <arielhodaya@gmail.com>
2025-06-30 00:25:14 +0300

    new

--- backEnd/scripts/app.js
+++ backEnd/scripts/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
 const URL = process.env.CONNECTION_STRING_ATLAS;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 const jobOffersRoute = require("../routers/jobOffers");
 const adminRoute = require("../routers/adminAuth");
